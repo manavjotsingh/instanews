ddd545c6917f1764bae92a41fc7d8fb74a085859
Manav Singh <manavjot13@gmail.com>
2019-10-31 14:47:20 -0700

    found the image url

--- js/script.js
+++ js/script.js
@@ -1,7 +1,14 @@
-console.log('start working');
 
 
 
+$.ajax({
+    method: 'get',
+    url: 'https://api.nytimes.com/svc/topstories/v2/home.json?api-key=Y0Po2jNwx2e3WMMp9UNMY6gHcB6PJe75',
+    dataType: 'json'
+})
+    .done(function ($data) { 
+const $articles = $data.results;
+console.log($articles[1].multimedia[4].url);
+    })
 
-
-
+      
