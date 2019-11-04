b46405daf8b8e0753b8ae9c2ed8da0c3143da05b
Manav Singh <manavjot13@gmail.com>
2019-11-03 16:58:49 -0800

    added custom selected

--- js/script.js
+++ js/script.js
@@ -1,32 +1,33 @@
 $(function() {
     $("#sections").on("change", function() {
-      console.log("selected");
-      const selected = $(this).val();
-      console.log(selected);
+      const $selected = $(this).val();
+      console.log($selected);
+      appendArticles($selected);
     });    
   });
 
-
-$.ajax({
-    method: 'get',
-    url: 'https://api.nytimes.com/svc/topstories/v2/${selected}.json?api-key=Y0Po2jNwx2e3WMMp9UNMY6gHcB6PJe75',
-    dataType: 'json'
-})
+function appendArticles($selected){
+    $.ajax({
+     method: 'get',
+     url: `https://api.nytimes.com/svc/topstories/v2/${$selected}.json?api-key=Y0Po2jNwx2e3WMMp9UNMY6gHcB6PJe75`,
+    })
     .done(function ($data) { 
-const $articles = $data.results;
-console.log($articles[1].multimedia[4].url);
-console.log($articles[1].multimedia[3].url);
-console.log($articles[1].multimedia[2].url);
-console.log($articles[1].multimedia[1].url);
-console.log($articles[1].multimedia[0].url);
-console.log($articles[1].multimedia[4].caption);
-console.log($articles[1].abstract);
-console.log($articles[1].title);
-console.log($articles[1].short_url);
+        console.log ($data.results)
+        const $articles = $data.results;
+
+        const $filteredMedia = $articles
+        .filter(function($filteredArticles) { 
+        return $filteredArticles.multimedia[4] !== undefined;
+                })
+        .slice (0,12);
+        $("#stories").append("");
+        $.each ($filteredMedia, function (index, value){
+            $("#stories").append(`<li class='article' style='background-image:url(${value.multimedia[4].url})'>
+            <p>${value.abstract}</p></li>`);
+            
+        })
     });
+    // console.log($filteredMedia)
+}
 
 
-
-
-$('#stories').append(`<li class='article' style='background-image:url(https://placeimg.com/1000/960/any)'>
-<p>Lorem ipsum dolor sit amet consectetur, adipisicing elit. Quas illum amet consequuntur saepe minima, repellat consectetur debitis placeat sunt, quibusdam architecto. Velit ipsa est aut deserunt facilis, perferendis et quaerat!</p></li>`); // adds content at the end in the selected elements
